a81e28d91f5050f2ced8cc0f76826c1a6de7665b
Nitinsaini1515 <gamern662@gmail.com>
2025-09-26 08:55:19 +0530

    Update next.config.mjs

--- Frontend/Nabhdesign3/next.config.mjs
+++ Frontend/Nabhdesign3/next.config.mjs
@@ -7,8 +7,6 @@ const nextConfig = {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
   },
-  output: 'export', // ✅ Static export
-  experimental: { appDir: true }, // required if using app/ directory
 }
 
 export default nextConfig
